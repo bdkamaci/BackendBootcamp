48ce95cf4120bb96c026bcb70035ed5a7f48676c
bdkamaci <burcudogakamaci@gmail.com>
2024-02-17 00:03:08 +0100

    Feat: Bootcamp Homework - Score Calculator using Input From User and Ternary Operator is added.

---
+++ src/Java101/scoreCalculator.java
@@ -0,0 +1,32 @@
+package Java101;
+
+import java.util.Scanner;
+
+public class scoreCalculator {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String[] subjects = {"Math", "Physics", "Chemistry", "Turkish", "History", "Music"};
+        int[] scores = new int[6];
+
+        for (int i = 0; i < 6; i++){
+            System.out.print("Enter your " + subjects[i] + " score: ");
+            scores[i] = input.nextInt();
+        }
+
+        double total = 0;
+        for (int i = 0; i < scores.length; i++) {
+            total += scores[i];
+            }
+
+        double gpa = (total / subjects.length);
+        System.out.println("GPA: " + gpa);
+
+        final String passage = gpa >= 60
+                ? "You have passed!"
+                : "You have failed!";
+
+        System.out.println(passage);
+        }
+    }
