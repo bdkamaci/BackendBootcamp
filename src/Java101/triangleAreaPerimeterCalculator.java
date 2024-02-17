27d870d9d61d58025d88955dfb00ddebe88c3aef
bdkamaci <burcudogakamaci@gmail.com>
2024-02-17 01:41:39 +0100

    Feat: Bootcamp Homework - Triangle Area and Perimeter Calculator is added by using Scanner.

---
+++ src/Java101/triangleAreaPerimeterCalculator.java
@@ -0,0 +1,29 @@
+package Java101;
+
+import java.util.Scanner;
+
+public class triangleAreaPerimeterCalculator {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int[] sides = new int[3];
+        double u = 0, area = 1;
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("Enter side " + i + " : ");
+            sides[i - 1] = input.nextInt();
+        }
+
+        for (int side : sides) {
+            u += side;
+        }
+
+        u /= 2;
+        for (int side : sides) {
+            area *= (u - side);
+        }
+
+        System.out.println("Perimeter: " + 2 * u);
+        System.out.println("Area: " + area);
+    }
+}
