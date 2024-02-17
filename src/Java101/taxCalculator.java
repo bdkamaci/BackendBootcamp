361497e5602dc12081cd988a71c7566b9162ad99
bdkamaci <burcudogakamaci@gmail.com>
2024-02-17 01:02:49 +0100

    Feat: Bootcamp Homework - Tax Calculator is added by using Scanner to get User Input.

---
+++ src/Java101/taxCalculator.java
@@ -0,0 +1,29 @@
+package Java101;
+
+import java.util.Scanner;
+
+public class taxCalculator {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the amount without tax: ");
+        double initial = input.nextDouble();
+
+        double amount = 0, taxPercentage = 0, taxAmount = 0;
+
+        if (initial > 1000){
+            taxPercentage += 0.08;
+        } else if ((initial > 0) && (initial <= 1000)) {
+            taxPercentage += 0.18;
+        }
+
+        taxAmount += (initial * taxPercentage);
+        amount += (initial + taxAmount);
+
+        System.out.println("Amount without Tax " + initial);
+        System.out.println("Tax Percentage: " + taxPercentage);
+        System.out.println("Tax Amount: " + taxAmount);
+        System.out.println("Amount with Tax: " + amount);
+    }
+}
