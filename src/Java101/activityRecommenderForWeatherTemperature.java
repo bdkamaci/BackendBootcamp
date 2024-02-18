f959342ef477d974ee66ab29075db0dcf2578f43
bdkamaci <burcudogakamaci@gmail.com>
2024-02-18 20:29:24 +0100

    Feat: A program that suggests events based on temperature was created using Java conditional statements.       Conditions:       - Suggest "Skiing" if the temperature is less than 5 degrees.       - If the temperature is between 5 and 15 degrees, suggest the "Cinema" activity.       - If the temperature is between 15 and 25 degrees, suggest the "Picnic" event.       - If the temperature is greater than 25 degrees, suggest the "Swimming" activity.

---
+++ src/Java101/activityRecommenderForWeatherTemperature.java
@@ -0,0 +1,27 @@
+package Java101;
+
+import java.util.Scanner;
+
+public class activityRecommenderForWeatherTemperature {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the temperature in numbers: ");
+        int temperature = input.nextInt();
+
+        if (temperature < 5) {
+            System.out.println("You can go skiing.");
+        } else if (temperature >= 5 && temperature <15) {
+            System.out.println("You can go see a movie.");
+        } else if (temperature >= 15 && temperature < 25) {
+            System.out.println("It's picnic time.");
+        } else if (temperature >= 25) {
+            System.out.println("You can go to swimming.");
+        } else {
+            System.out.println("You have entered something wrong. Temperature is not correct. Operation cancelled.");
+        }
+
+    }
+
+}
