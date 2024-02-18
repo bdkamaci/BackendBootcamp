fee7321f02f4a01b5e662478f2be8832de03221a
bdkamaci <burcudogakamaci@gmail.com>
2024-02-18 21:16:37 +0100

    Feat: A program was created that sorts 3 numbers entered using Java conditional statements, from smallest to largest to smallest.

---
+++ src/Java101/Sorter.java
@@ -0,0 +1,31 @@
+package Java101;
+
+import java.util.Scanner;
+
+public class Sorter {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] numbers = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter a number: ");
+            numbers[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length - 1; j++) {
+                if (numbers[j] < numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                    }
+            }
+        }
+
+        for (int elem : numbers) {
+            System.out.println(elem);
+        }
+    }
+}
