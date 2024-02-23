f5a7a7d766cbb7ccb55076a8bbcb119f11b5e27e
bdkamaci <burcudogakamaci@gmail.com>
2024-02-23 13:11:33 +0100

    Fix: PrimeNumbers - Add User Input for initialization.

--- src/java101/PrimeNumbers.java
+++ src/java101/PrimeNumbers.java
@@ -1,13 +1,22 @@
 package java101;
 
+import java.util.Scanner;
+
 public class PrimeNumbers {
     public static void main(String[] args) {
 
-        // Variable Declarations
-        int start = 1, finish = 100;
+        // Scanner Declaration
+        Scanner input = new Scanner(System.in);
+
+        // Variable Declarations and User Input
+        int start, finish;
+        System.out.print("Enter initial number: ");
+        start = input.nextInt();
+        System.out.print("Enter last number: ");
+        finish = input.nextInt();
 
         // Printing Prime Numbers
-        System.out.println("Prime numbers between 1-100: ");
+        System.out.println("Prime numbers between " + start + " - " + finish + ": ");
         for (int i = start; i <= finish; i++) {
             if (isPrime(i)) {
                 System.out.print(i + " ");
